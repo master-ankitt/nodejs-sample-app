59fd1867cde30d7eb9df174e3174b9924ae354de
Ankit <125477559+master-ankitt@users.noreply.github.com>
2025-10-02 22:29:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'logoswayatt.png'));
+  res.sendFile(path.join(__dirname, 'grras.png'));
 });
 
 app.listen(3000, () => {
